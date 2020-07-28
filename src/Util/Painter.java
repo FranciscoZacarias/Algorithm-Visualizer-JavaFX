c0ebb40d5710bfda0f69cc61eddf9a81ac79e578
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2020-07-28 06:32:18 +0100

    painter to handle tile drawings

---
+++ src/Util/Painter.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import Model.Grid;
+import Model.Tile;
+import PathfindingStrategy.PathfindingStrategy;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author frank
+ */
+public final class Painter
+{
+    private static final Painter INSTANCE = new Painter();
+    private Executor executor;
+    
+    private Painter()
+    {
+        executor = Executors.newSingleThreadExecutor();
+    }
+    
+    public static Painter getInstance()
+    {
+        return INSTANCE;
+    }
+    
+    /**
+     * Draws a path from root to target, given a list of tiles
+     * @param path
+     * @param target
+     * @param root
+     */
+    public void drawPath(List<Tile> path, Tile target, Tile root) 
+    {
+        this.executor.execute(
+        () ->
+        {
+            path.stream().filter((tile) -> !(tile == target || tile == root)).map((tile) ->
+            {
+                tile.setAttributes(Tile.Type.PATH, tile.getWeight());
+                return tile;                
+            }).forEachOrdered((_item) ->
+            {
+                try
+                {
+                    Thread.sleep(20);
+                }
+                catch (InterruptedException ex)
+                {
+                    Logger.getLogger(Grid.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+        });
+    }
+    
+    /**
+     * Returns a runnable object that colors a single Tile and sleeps for 'sleep' ms
+     * @param tile Tile to color
+     * @param target We pass this to avoid overriding it
+     * @param root We pass this to avoid overriding it
+     * @param color Color to paint the tile
+     * @param sleep time to wait before next task in thread
+     */
+    public void drawTile(Tile tile, Tile target, Tile root, Tile.Type color, long sleep)
+    {
+        this.executor.execute(()->
+        {
+            if(tile != target && tile != root)
+                tile.setAttributes(color, tile.getWeight());
+           
+            try
+            {
+                Thread.sleep(sleep);
+            }
+            catch (InterruptedException ex)
+            {
+                Logger.getLogger(PathfindingStrategy.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+}
