bf1181105fcc96859a6bff6c405f894fdaf3f6d5
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2020-09-21 14:19:16 +0100

    sidewinder strategy

---
+++ src/Strategy/MazeGenerationStrategy/SidewinderStrategy.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strategy.MazeGenerationStrategy;
+
+import Model.Grid;
+
+/**
+ *
+ * @author frank
+ */
+public class SidewinderStrategy extends MazeGenerationStrategy
+{
+
+    public SidewinderStrategy()
+    {
+        super();
+    }
+
+    @Override
+    public void algorithm(Grid model)
+    {
+        
+    }
+}
